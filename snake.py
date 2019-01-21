b9db2bb7a2f0eb4abb3d8c2df2189f23f0a1ceb8
Sudeep Gumaste <sudeepgumaste19@gmail.com>
2019-01-22 00:11:10 +0530

    screwed length updation

--- snake.py
+++ snake.py
@@ -26,15 +26,17 @@ class food():
         self.pos = point(random.randint(0,23),random.randint(0,23))
 
     def generateFood(self,canvas):
-        self.pos.x = random.randint(0,22)*30
-        self.pos.y = random.randint(0,22)*30
+        self.pos.x = random.randint(0,22)*blockSize
+        self.pos.y = random.randint(0,22)*blockSize
         canvas = cv2.rectangle(canvas, (self.pos.x , self.pos.y) , (self.pos.x + blockSize ,self.pos.y + blockSize), (255,0,0), -1)
         return canvas
 
     def posFood(self,canvas):
+        #draw the food again
         canvas = cv2.rectangle(canvas, (self.pos.x , self.pos.y) , (self.pos.x + blockSize ,self.pos.y + blockSize), (255,0,0), -1)
 
     def eatFood(self,snake,canvas):
+        #checking if snake has eaten the food
         if snake.bodyArray[0] == self.pos:
             canvas = cv2.rectangle(canvas, (self.pos.x , self.pos.y) , (self.pos.x + blockSize ,self.pos.y + blockSize), (20,20,20), -1)
             self.generateFood(canvas)
@@ -52,49 +54,63 @@ class snake():
         }
         self.curDir='right'
         self.F = food()
+        self.Die = False
 
     
     def update(self):
         for i in range(len(self.bodyArray)):
             self.bodyArray[i] = self.bodyArray[i] + self.dirs[self.curDir]
             print(self.bodyArray[i].x,self.bodyArray[i].y)
-            
-        time.sleep(0.20)
-        
-    def getDir(self,keyPress):
-        print(keyPress)
+        self.die()
+        #time.sleep(0.25)
+
+
+    def die(self):
+        if self.bodyArray[0].x < 0 or self.bodyArray[0].x > 630 or self.bodyArray[0].y < 0 or self.bodyArray[0].y > 630:
+            print('dead')
+            self.Die = True
+
 
     def display(self):
         frame = 1
-        while True:
+        while not(self.Die):
             canvas=20*np.ones((frameSize,frameSize),np.uint8)
             canvas = cv2.cvtColor(canvas,cv2.COLOR_GRAY2BGR)
             
             if frame == 1:
+                #when the game starts
                 self.F.generateFood(canvas)
                 frame+=1
             
+            #drawing food on canvas
             self.F.posFood(canvas)
 
+
+            #drawing snake on canvas
             for blk in self.bodyArray:
                 cv2.rectangle(canvas, (blk.x , blk.y) , (blk.x + blockSize ,blk.y + blockSize), (255,255,255), -1)
             cv2.imshow("game",canvas)
+
+            fd = self.F.eatFood(self,canvas)
+            prevPos = self.bodyArray[-1]
             self.update()
+            if fd:
+                self.bodyArray.append(prevPos)
             
 
 
             #capturing key presses
-            keyPress = cv2.waitKeyEx(4)
+            keyPress = cv2.waitKeyEx(250)
             if keyPress == 27:
                 break
             
-            elif keyPress == 65364 and self.curDir != 'up':
+            elif keyPress == 65364 and (self.curDir != 'up' or self.curDir != 'down'):
                 self.curDir = 'down'
-            elif keyPress == 65361 and self.curDir != 'right':
+            elif keyPress == 65361 and (self.curDir != 'right' or self.curDir != 'left'):
                 self.curDir = 'left'
-            elif keyPress == 65363 and self.curDir != 'left':
+            elif keyPress == 65363 and (self.curDir != 'left' or self.curDir != 'right'):
                 self.curDir = 'right'
-            elif keyPress == 65362 and self.curDir != 'down':
+            elif keyPress == 65362 and (self.curDir != 'down' or self.curDir != 'up'):
                 self.curDir = 'up'
             print(keyPress)
             
